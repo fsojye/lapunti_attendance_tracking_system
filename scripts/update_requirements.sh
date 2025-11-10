e3737fc458fd1d61a6204ead0ec2b122dc642a1b
Josf <josf@yobi.app>
2025-11-10 18:11:43 +0800

    update script for requirements locking

--- scripts/update_requirements.sh
+++ scripts/update_requirements.sh
@@ -3,4 +3,9 @@
 APP_DIR="src"
 
 uv pip compile $APP_DIR/requirements.in -o $APP_DIR/requirements.txt \
-    --generate-hashes
+    --generate-hashes \
+    --no-cache-dir \
+    --no-header \
+    --no-emit-index-url \
+    --emit-build-options \
+    --strip-extras
