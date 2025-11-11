ea68320d8bfb86ec66317a6190f111e29cfc3b5e
Josf <josf@yobi.app>
2025-11-12 02:12:32 +0800

    format code

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -11,5 +11,7 @@ class TestApp:
         assert app.title == "Lapunti Attendance Tracking System API"
         assert app.version == "1.0"
 
-    def test_app_routes_should_include_attendance_images_endpoint(self, method_path):
+    def test_app_routes_when_initialised_should_include_attendance_images_route(
+        self, method_path
+    ):
         assert ("POST", "/attendance/images") in method_path
